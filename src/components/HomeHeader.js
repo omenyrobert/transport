549e2f0adfe57f1cb981cc1cf19da6caa504cf1a
Jim Junior <jimjunior854@gmail.com>
2023-10-05 17:23:57 +0300

    Remove cycle imports

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,7 +1,6 @@
 
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
-import Navigation from '../Navigation';
 
 import { useNavigation } from "@react-navigation/native";
 const HomeHeader = () => {
@@ -9,17 +8,17 @@ const HomeHeader = () => {
   return (
     <View style={tw`flex-row px-3 pb-5 pt-8 justify-between`} >
       {/* Logo */}
-      <TouchableOpacity onPress={()=> navigation.navigate('Home')}>
-      <Image  source={require('../../assets/navlogo.png')} style={{ width: 150, height: 40 }} />
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Image source={require('../../assets/navlogo.png')} style={{ width: 150, height: 40 }} />
       </TouchableOpacity>
-     
+
 
       {/* Profile Avatar */}
-      <TouchableOpacity onPress={()=> navigation.navigate('About')}>
+      <TouchableOpacity onPress={() => navigation.navigate('About')}>
 
-      <View style={tw`bg-amber-600  rounded-full h-8 w-8 pl-2`}>
-        <Text style={tw`text-white font-bold text-xl`} >O</Text>
-      </View>
+        <View style={tw`bg-amber-600  rounded-full h-8 w-8 pl-2`}>
+          <Text style={tw`text-white font-bold text-xl`} >O</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
